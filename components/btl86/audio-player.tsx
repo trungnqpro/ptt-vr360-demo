0b9c00a4c1bbfb401d50012513c06cc9f26a81d9
Nguyen Trung <nguyentrung@Trungs-iMac.local>
2023-10-25 00:26:12 +0700

    fix build err audio ref

--- components/btl86/audio-player.tsx
+++ components/btl86/audio-player.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useRef, useState } from 'react';
 
 interface AudioPlayerProps {
   src: string;
@@ -13,23 +12,23 @@ export default function AudioPlayer({
   isPlayAudio,
   setPlayAudio
 }: AudioPlayerProps) {
-  const audioRef = useRef();
-  const [duration, setDuration] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
+  // const audioRef = useRef();
+  // const [duration, setDuration] = useState(0);
+  // const [currentTime, setCurrentTime] = useState(0);
 
 
-  const handleLoadedData = () => {
-    console.log("[handleLoadedData]")
-    if (isPlayAudio) audioRef.current.play();
-  };
+  // const handleLoadedData = () => {
+  //   console.log("[handleLoadedData]")
+  //   if (isPlayAudio) audioRef?.current?.play();
+  // };
   return (
     <div>
         <audio
-          ref={audioRef}
+          // ref={audioRef}
           src={src}
           controls
-          onLoadedData={handleLoadedData}
-          onTimeUpdate={() => setCurrentTime(audioRef?.current?.currentTime)}
+          // onLoadedData={handleLoadedData}
+          // onTimeUpdate={() => setCurrentTime(audioRef?.current?.currentTime)}
           onEnded={() => setPlayAudio(false)}
         />
     </div>
